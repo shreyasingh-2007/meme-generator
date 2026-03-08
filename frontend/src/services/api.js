6c5ddd7f89c99f8acf6ff268770d501e4c7f8f17
shreyasingh-2007 <shreyasinghs2007@gmail.com>
2026-03-08 16:24:45 +0530

    Updated API URL for deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://meme-generator-backend-bnvm.onrender.com/api';
 
 export const getMemes = async () => {
     const response = await axios.get(`${API_URL}/memes`);
